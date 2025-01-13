e4cbd9c10c649df13252d848b1f995293770aa95
A.F. Adib <a.f.adib36@gmail.com>
2025-01-14 02:43:25 +0600

    edit person view

---
+++ FaceFacts_SwiftData/EditPersonView.swift
@@ -0,0 +1,35 @@
+//
+//  EditPersonView.swift
+//  FaceFacts_SwiftData
+//
+//  Created by A.F. Adib on 14/1/25.
+//
+
+import SwiftUI
+
+struct EditPersonView: View {
+    
+    @Bindable var person: Person
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Name", text: $person.name)
+                    .textContentType(.name)
+                
+                TextField("Email Address", text: $person.emailAddress)
+                    .textContentType(.emailAddress)
+            }
+            
+            Section("Notes") {
+                TextField("Details about this person", text: $person.details, axis: .vertical)
+            }
+        }
+        .navigationTitle("Edit Person")
+     //   .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    EditPersonView()
+}
