33cf637e40ea3fe1995deed630e90e457dc9763c
A.F. Adib <a.f.adib36@gmail.com>
2025-01-23 23:18:54 +0600

    modified

--- FaceFacts_SwiftData/Event.swift
+++ FaceFacts_SwiftData/Event.swift
@@ -12,4 +12,10 @@ import SwiftData
 class Event {
     var name: String
     var location: String
+    var people = [Person]()
+    
+    init(name: String, location: String) {
+        self.name = name
+        self.location = location
+    }
 }
