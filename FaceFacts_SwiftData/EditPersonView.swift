37c7b8c0f898f98803e47760312ca32f8235dedf
A.F. Adib <a.f.adib36@gmail.com>
2025-01-24 19:34:32 +0600

    picker added

--- FaceFacts_SwiftData/EditPersonView.swift
+++ FaceFacts_SwiftData/EditPersonView.swift
@@ -6,10 +6,17 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct EditPersonView: View {
     
     @Bindable var person: Person
+    
+    @Query(sort: [
+        SortDescriptor(\Event.name),
+        SortDescriptor(\Event.location)
+    ]) var events: [Event]
+    
     
     var body: some View {
         
@@ -23,12 +30,31 @@ struct EditPersonView: View {
                     .textInputAutocapitalization(.never)
             }
             
+            Section("Where did you meet them?") {
+                Picker("Met at", selection: $person.metAt) {
+                    Text("Unknown event")
+                    
+                    if events.isEmpty == false {
+                        Divider()
+                        
+                        ForEach(events) { event in
+                            Text(event.name)
+                        }
+                    }
+                }
+                Button("Add a new event", action: addEvent)
+            }
+            
             Section("Notes") {
                 TextField("Details about this person", text: $person.details, axis: .vertical)
             }
         }
         .navigationTitle("Edit Person")
         .navigationBarTitleDisplayMode(.inline)
+    }
+    
+    func addEvent() {
+        
     }
 }
 //
